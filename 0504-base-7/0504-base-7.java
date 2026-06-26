e8bf0fc037e7e51fe030a10486f9e5c8a136cb80
Saransh Bhargava <bhargavasaransh037@gmail.com>
2026-06-27 01:24:26 +0530

    Time: 0 ms (100%), Space: 42.3 MB (96.4%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,14 +1,6 @@
 class Solution {
     public String convertToBase7(int num) {
-        int absoluteNum = Math.abs(num);
-        if(num == 0) return "0";
-        String result = "";
-        while(absoluteNum != 0){
-            int digit = absoluteNum % 7;
-            result = String.valueOf(digit) + result;
-            absoluteNum /= 7;
-        }
-        if(num < 0) return "-" + result;
-        return result;
+        String str = Integer.toString(num , 7);
+        return str;
     }
 }
