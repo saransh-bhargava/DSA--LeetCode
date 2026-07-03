4d969b38e60a9aaaecb39dd9df431ab4ecced076
Saransh Bhargava <bhargavasaransh037@gmail.com>
2026-07-03 22:53:05 +0530

    Time: 1 ms (70.5%), Space: 42.8 MB (7.66%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,24 +1,36 @@
 class Solution {
-    public static List<Integer> pascal(int rowIndex, int row , List<Integer> result){
-        if(row == rowIndex){
-            return result;
-        }
+    // public static List<Integer> pascal(int rowIndex, int row, List<Integer> result) {
+    //     if (row == rowIndex) {
+    //         return result;
+    //     }
 
-        List<Integer> list = new ArrayList<>();
+    //     List<Integer> list = new ArrayList<>();
 
-        list.add(1);
-        for(int i = 0; i < result.size() - 1; i++){
-            list.add(result.get(i) + result.get(i + 1));
-        }
+    //     list.add(1);
+    //     for (int i = 0; i < result.size() - 1; i++) {
+    //         list.add(result.get(i) + result.get(i + 1));
+    //     }
 
-        list.add(1);
-        return pascal(rowIndex, row + 1, list);
-    }
+    //     list.add(1);
+    //     return pascal(rowIndex, row + 1, list);
+    // }
+
     public List<Integer> getRow(int rowIndex) {
         List<Integer> result = new ArrayList<>();
 
         result.add(1);
 
-        return pascal(rowIndex, 0 , result);
+        for(int i = 1; i <= rowIndex; i++){
+            List<Integer> list = new ArrayList<>();
+
+            list.add(1);
+            for(int j = 1; j < result.size(); j++){
+                list.add(result.get(j) + result.get(j - 1));
+            }
+            list.add(1);
+            result = list;
+        }
+        return result;
+        
     }
 }
