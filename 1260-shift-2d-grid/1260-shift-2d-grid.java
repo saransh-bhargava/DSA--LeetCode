b35c23e517a8ffb8726bc46802793c86fed5c1db
Saransh Bhargava <bhargavasaransh037@gmail.com>
2026-07-20 10:25:12 +0530

    Time: 5 ms (83.45%), Space: 46.9 MB (96.55%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,12 +1,12 @@
 class Solution {
-    public static void reverse(int[] arr, int left, int right){
+    public static void reverse(int[] arr, int i, int j){
         
-        while(left < right){
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
         }
         
     }
@@ -14,27 +14,27 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int n = grid.length;
         int m = grid[0].length;
+        int size = n * m;
 
-        int[] arr = new int[n * m];
+        int[] arr = new int[size];
         int idx = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 arr[idx++] = grid[i][j];
-                
             }
         }
 
-        k = k % (n * m) ;
+        k %= size;
 
-        reverse(arr, 0, (n * m) - 1);
+        reverse(arr, 0, size - 1);
         reverse(arr, 0, k - 1);
-        reverse(arr, k, (n * m) - 1);
+        reverse(arr, k, size - 1);
 
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
         
 
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0; i < size; i++){
             curr.add(arr[i]);
             if((i + 1) % m == 0){
                 result.add(new ArrayList<>(curr));
