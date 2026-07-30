2234517d31de5ceb625c46e119c08b32ff4e9e78
Saransh Bhargava <bhargavasaransh037@gmail.com>
2026-07-31 00:05:43 +0530

    Time: 8 ms (63.77%), Space: 45.9 MB (15.66%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -2,28 +2,20 @@ class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int n = nums.length;
 
-        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         for (int val : nums) {
             map.put(val, map.getOrDefault(val, 0) + 1);
         }
 
-        int[] temp = new int[map.size()];
+        
+        int freq = 0;
+        for (int val : map.keySet()) {
+            int temp = freq;
+            freq += map.get(val);
+            map.put(val,temp);
+        }
 
         int index = 0;
-        for (int val : map.keySet()) {
-            temp[index++] = val;
-        }
-
-        Arrays.sort(temp);
-
-        int freq = 0;
-        for (int val : temp) {
-            int temp1 = freq;
-            freq += map.get(val);
-            map.put(val, temp1);
-        }
-
-        index = 0;
         for (int val : nums) {
             nums[index++] = map.get(val);
         }
