219016e2d573cc9a7127e9b0e952a0ea406c6a78
Saransh Bhargava <bhargavasaransh037@gmail.com>
2026-08-08 18:35:55 +0530

    Time: 0 ms (100%), Space: 44.7 MB (48.84%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -9,14 +9,14 @@ class Solution {
             return n;
         }
 
-       while(left < right){
+       while(left <= right){
          int mid = left + (right - left) / 2;
          if(nums[mid] == target){
             return mid;
          }else if(nums[mid] < target){
             left = mid + 1;
          }else{
-            right = right - 1;
+            right = mid - 1;
          }
        }
        return left;
